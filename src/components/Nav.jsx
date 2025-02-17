8c42fa2bebc9c0941d0f2a242159eaab40802d1d
Tushar Srivastava <tusharsrivastava2003@gmail.com>
2025-02-18 00:38:46 +0530

    feat: add navigation element

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <div className="w-full px-8">
+      <div className="bg-blue-light flex flex-row justify-evenly max-w-xl mx-auto mt-14 py-4 rounded-4xl">
+        <button className="font-roboto text-sm md:text-[18px] text-text-black hover:text-blue cursor-pointer">
+          Refer
+        </button>
+        <button className="font-roboto text-sm md:text-[18px] text-text-black hover:text-blue cursor-pointer">
+          Benefits
+        </button>
+        <button className="font-roboto text-sm md:text-[18px] text-text-black hover:text-blue cursor-pointer">
+          FAQs
+        </button>
+        <button className="font-roboto text-sm md:text-[18px] text-text-black hover:text-blue cursor-pointer">
+          Support
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
