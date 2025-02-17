43a0a6716472f90dafa801ea96e7afd089a0e0b8
Tushar Srivastava <tusharsrivastava2003@gmail.com>
2025-02-17 23:55:49 +0530

    feat: add Banner component with text and contact button

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="w-full flex flex-row justify-center text-center bg-blue-light py-2 px-4">
+        <p className='text-left'>Navigate your ideal career path with tailored expert advice.</p>
+        <button className='text-blue ml-2 text-nowrap'>Contact Expert</button>
+    </div>
+  )
+}
+
+export default Banner
